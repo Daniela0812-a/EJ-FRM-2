bd66de7c9580aa8963273f9912978646f09542b9
Lyly Tenorio <ldtenorio23a@udenar.edu.co>
2025-11-28 08:41:34 -0500

    Ejemplo de php

---
+++ server/save.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola mundo desde PHP </h1>
+    <?php
+        $a = 100;
+        $b = 2;
+        $c = $a * $b;
+        for ($i=0; $i < $c; $i++){
+        echo '<h> Hola mundo desde PHP'.$c. '</h1>';
+        }
+    ?>
+
+</body>
+</html>
